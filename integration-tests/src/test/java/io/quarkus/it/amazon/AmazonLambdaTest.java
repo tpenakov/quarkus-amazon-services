485b1292cb4341af5f1f5a8febf7b991c9e5e136
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2023-11-24 23:12:33 +0100

    use init script

--- integration-tests/src/test/java/io/quarkus/it/amazon/AmazonLambdaTest.java
+++ integration-tests/src/test/java/io/quarkus/it/amazon/AmazonLambdaTest.java
@@ -2,65 +2,23 @@ package io.quarkus.it.amazon;
 
 import static org.hamcrest.Matchers.is;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Optional;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ArrayUtils;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.it.amazon.lambda.LambdaResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 
 @QuarkusTest
 public class AmazonLambdaTest {
 
-    byte[] helloLambdaZipArchive;
-
-    @BeforeAll
-    public void setup() throws IOException {
-
-        // zip the index.js file
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ZipOutputStream zos = new ZipOutputStream(baos)) {
-            String filename = "index.js";
-            ZipEntry entry = new ZipEntry(filename);
-            zos.putNextEntry(entry);
-            zos.write(IOUtils
-                    .toByteArray(Optional.ofNullable(getClass().getResourceAsStream("/lambda/index.js"))
-                            .orElseThrow()));
-            zos.closeEntry();
-            zos.close();
-
-            // this is the zip file as byte[]
-            helloLambdaZipArchive = baos.toByteArray();
-        }
-    }
+    public static final String LAMBDA_NAME = "localstack-lambda-hello";
 
     @Test
     public void testAsync() {
-        RestAssured
-                .given()
-                .when()
-                .body(helloLambdaZipArchive)
-                .post("/test/lambda/async")
-                .then()
-                .body(is(LambdaResource.OK));
+        RestAssured.given().when().get("/test/lambda/async").then().body(is(LAMBDA_NAME));
     }
 
     @Test
     public void testBlocking() {
-        RestAssured
-                .given()
-                .when()
-                .body(helloLambdaZipArchive)
-                .post("/test/lambda/blocking").then()
-                .body(is(LambdaResource.OK));
+        RestAssured.given().when().get("/test/lambda/blocking").then().body(is(LAMBDA_NAME));
     }
 }
--- integration-tests/src/test/resources/lambda/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    let body = JSON.parse(event.body)
-    const name = body.name
-    const response = {
-        statusCode: 200,
-        body: "Hello " + name + "!",
-    };
-    return response;
-};
